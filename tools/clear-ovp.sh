08d0b11d2489193c47ab2a5e6f76140f28ca5569
Giede van Gils <giedevangils@proton.me>
2024-11-08 09:16:28 +0100

    update clear script to reflect use of counters

--- tools/clear-ovp.sh
+++ tools/clear-ovp.sh
@@ -5,10 +5,12 @@ if [ "$CONTEXT" != "default" ]; then
 	exit 1
 fi
 
-read -p "This will delete the ovp & bijlage-api databases. it'll also clear the testtool of messages and opdrachten. Continue? (y/n) " choice
+read -p "This will delete the bijlage-api databases. It'll clear all but the counters from the opdrachtverstrekking db. It'll also clear the testtool of messages and opdrachten. Continue? (y/n) " choice
 
 case "$choice" in
-	y|Y ) mongosh opdrachtverstrekking --eval 'db.dropDatabase()' --quiet; mongosh bijlage --eval 'db.dropDatabase()' --quiet; mongosh testtool --eval 'db.message.deleteMany({}); db.opdracht.deleteMany({});' --quiet;;
+	y|Y ) mongosh opdrachtverstrekking --eval 'db.opdracht.drop(); db.message.drop(); db.opdrachthoofdnet.drop(); db.levering.drop(); db.placeholder_opdracht.drop();' --quiet; \
+	     	mongosh bijlage --eval 'db.dropDatabase()' --quiet; \
+			 	mongosh testtool --eval 'db.message.deleteMany({}); db.opdracht.deleteMany({});' --quiet;;
 	* ) echo "no action taken"; exit 0;;
 esac
 
