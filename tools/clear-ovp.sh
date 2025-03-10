7eb729175b23f0434f1d84f32408025dec448cba
Giede van Gils <giedevangils@proton.me>
2025-03-10 15:31:07 +0100

    refactor clear opv to multiple lines

--- tools/clear-ovp.sh
+++ tools/clear-ovp.sh
@@ -5,11 +5,15 @@ if [ "$CONTEXT" != "default" ]; then
 	exit 1
 fi
 
-read -p "This will delete the bijlage-api databases. It'll clear all but the counters from the opdrachtverstrekking db. It'll also clear the testtool of messages and opdrachten. Continue? (y/n) " choice
+read -p "This will delete the bijlage-api databases. It'll clear all but the counters from the opdrachtverstrekking db. It'll also clear the testtool of messages, opdrachten, and leveringen. Continue? (y/n) " choice
 
 case "$choice" in
-	y|Y ) mongosh opdrachtverstrekking --eval 'db.opdracht.drop(); db.message.drop(); db.opdrachthoofdnet.drop(); db.levering.drop(); db.placeholder_opdracht.drop();' --quiet; \
-	     	mongosh bijlage --eval 'db.dropDatabase()' --quiet; \
-			 	mongosh testtool --eval 'db.message.deleteMany({}); db.opdracht.deleteMany({});' --quiet;;
+	y|Y ) echo "Clearing databases"; \
+				echo Opdrachtverstrekking; \
+				mongosh opdrachtverstrekking --eval 'db.opdracht.drop(); db.message.drop(); db.opdrachthoofdnet.drop(); db.levering.drop(); db.placeholder_opdracht.drop();' --quiet; \
+				echo Bijlagen; \
+	     	mongosh bijlage --eval 'db.bijlage.drop()' --quiet; \
+				echo Testtool; \
+			 	mongosh testtool --eval 'db.message.drop(); db.opdracht.drop(); db.levering.drop()' --quiet;;
 	* ) echo "no action taken"; exit 0;;
 esac
